2de17367d2214e676ccda4755c0011c797fae920
Jeferson859 <132462958+Jeferson859@users.noreply.github.com>
2026-03-16 22:45:48 -0300

    Update atualizar_ofertas.js

--- atualizar_ofertas.js
+++ atualizar_ofertas.js
@@ -1,26 +1,2 @@
-const fs = require('fs');
-
-async function buscarOfertas() {
-    const token = process.env.LOMADEE_TOKEN;
-    const sourceId = '6ff2699e-ceaa-4fad-a58a-8b91f885485f'; 
-    const url = `https://api.lomadee.com/v3/${token}/offer/_bestsellers?sourceId=${sourceId}`;
-
-    try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-        if (data.offers && data.offers.length > 0) {
-            const ofertas = data.offers.map(o => ({
-                id: o.id,
-                titulo: o.name,
-                preco: o.price.toLocaleString('pt-BR', { minimumFractionDigits: 2 }),
-                link: o.link,
-                img: o.thumbnail
-            }));
-            fs.writeFileSync('ofertas.json', JSON.stringify(ofertas, null, 2));
-        }
-    } catch (error) {
-        console.log('Aguardando aprovação da API...');
-    }
-}
-buscarOfertas();
+console.log("Iniciando busca...");
+// O código real da Lomadee entrará aqui depois da aprovação
